5c4d26228ccd83f91105e9b6a7c0ec10e25b4a47
Vetusia <ivplportfolio@gmail.com>
2021-12-09 21:46:58 +0300

    test

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-for(let i = 0; i < 2; i++){
-    for(let k = 0; )
-}
+// function num(addNum){
+//     return function(targetNum){
+//         return targetNum + addNum;
+//     }
+// }
+// let addFive = addNum(5);
+// let rez = addFive(10);
+// console.log(rez);
+
+// function gooseFilter (birds) {
+//   var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+   
+//    return birds.filter(item => !geese.includes(item));
+// };
+
+// console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
+
+ 
